36dc1aa8e6dad7c9c345c79e8b92c8b24201deea
Karen-Rdz <31635951+Karen-Rdz@users.noreply.github.com>
2022-07-08 12:06:21 -0700

    Get distance and duration from origin to destination

--- src/components/NumberStops/NumberStops.jsx
+++ src/components/NumberStops/NumberStops.jsx
@@ -6,13 +6,19 @@ import "./NumberStops.css";
 import { LoadScript } from "@react-google-maps/api";
 import Map2 from "../Map2/Map2";
 import InitMap from "../Map3/Map3";
+import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRor9dsPY8WcfhoMvQM7bHbEXo-NsiUGc&libraries=geometry"></script>;
+const lib = ["places", "geometry"];
 
 export default function NumberStops() {
-  const [origin, setOrigin] = React.useState("");
-  const [destination, setDestination] = React.useState("");
-  const [resultsReady, setResultsReady] = React.useState(false);
-  const [originName, setOriginName] = React.useState("");
-  const [destinationName, setDestinationName] = React.useState("");
+  "use strict";
+  const [originInput, setOriginInput] = React.useState("");
+  const [destinationInput, setDestinationInput] = React.useState("");
+  const [origin, setOrigin] = React.useState();
+  const [destination, setDestination] = React.useState();
+  const [originName, setOriginName] = React.useState("Origin");
+  const [destinationName, setDestinationName] = React.useState("Destination");
 
   const location = {
     address: "1 Hacker Way, Menlo Park, CA 94025.",
@@ -20,27 +26,28 @@ export default function NumberStops() {
     lng: -122.14830386019975,
   };
 
-  const lib = ["places"];
   const key = "AIzaSyBRor9dsPY8WcfhoMvQM7bHbEXo-NsiUGc";
 
   function handleTrip() {
     var configOrigin = {
       method: "get",
-      url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${origin}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${key}`,
+      url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${originInput}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${key}`,
       headers: {},
     };
 
     var configDestination = {
       method: "get",
-      url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${destination}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${key}`,
+      url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${destinationInput}&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${key}`,
       headers: {},
     };
 
     axios(configOrigin)
       .then(function (responseOrigin) {
         setOriginName(responseOrigin.data.candidates[0].name);
+        setOrigin(responseOrigin.data.candidates[0]);
         console.log(
-          JSON.stringify(responseOrigin.data.candidates[0].geometry.location)
+          // JSON.stringify(responseOrigin.data.candidates[0].geometry.location)
+          origin
         );
       })
       .catch(function (error) {
@@ -50,16 +57,43 @@ export default function NumberStops() {
     axios(configDestination)
       .then(function (responseDestination) {
         setDestinationName(responseDestination.data.candidates[0].name);
+        setDestination(responseDestination.data.candidates[0]);
         console.log(
-          JSON.stringify(
-            responseDestination.data.candidates[0].geometry.location
-          )
+          destination
+          // JSON.stringify(
+          //   responseDestination.data.candidates[0].geometry.location
+          // )
         );
       })
       .catch(function (error) {
         console.log(error);
       });
-    setResultsReady(true);
+  }
+
+  function calculateDistance() {
+    // console.log(origin);
+    let destinationLatLng = new window.google.maps.LatLng(
+      destination.geometry.location.lat,
+      destination.geometry.location.log
+    );
+    let originLatLng = new window.google.maps.LatLng(
+      origin.geometry.location.lat,
+      origin.geometry.location.long
+    );
+    let service = new window.google.maps.DistanceMatrixService();
+    console.log(originLatLng);
+    service.getDistanceMatrix(
+      {
+        origins: [originName],
+        destinations: [destinationName],
+        travelMode: "DRIVING",
+      },
+      callback
+    );
+    function callback(response, status) {
+      // console.log(response)
+      console.log(response.rows[0].elements[0]);
+    }
   }
 
   return (
@@ -70,22 +104,25 @@ export default function NumberStops() {
           <input
             type="text"
             placeholder="Origin"
-            onChange={(event) => setOrigin(event.target.value)}
+            onChange={(event) => setOriginInput(event.target.value)}
           />
           <input
             type="text"
             placeholder="Destination"
-            onChange={(event) => setDestination(event.target.value)}
+            onChange={(event) => setDestinationInput(event.target.value)}
           />
           <button type="Submit" onClick={handleTrip}>
             Submit
           </button>
         </div>
-        <div className="category">
-          <input type="radio" /> Time
-          <input type="radio" /> Distance
-          <input type="radio" /> Fuel
-        </div>
+        <LoadScript googleMapsApiKey={key} libraries={lib}>
+          <div className="category">
+            <input type="radio" /> Time
+            <input type="radio" onClick={calculateDistance} /> Distance
+            <input type="radio" /> Fuel
+          </div>
+        </LoadScript>
+
         <p>
           {originName} , {destinationName}
         </p>
--- src/components/Places/Places.jsx
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-import "./Places.css";
-
-export default function Places({ origin, destination }) {}
