498471363dfb9cb8cb48f0782f804b925203bdb8
Karen-Rdz <31635951+Karen-Rdz@users.noreply.github.com>
2022-07-11 16:02:59 -0700

    Update ReadMe

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,9 +5,12 @@ import {
   Marker,
   DirectionsService,
   DirectionsRenderer,
+  StandaloneSearchBox,
 } from "@react-google-maps/api";
 
 function Map({ origin, destination }) {
+  const key = "AIzaSyBRor9dsPY8WcfhoMvQM7bHbEXo-NsiUGc";
+  const lib = ["places", "geometry", "drawing"];
   const [response, setResponse] = React.useState();
 
   let count = React.useRef(0);
@@ -23,8 +26,12 @@ function Map({ origin, destination }) {
     }
   };
 
+  const onLoad = (ref) => (this.searchBox = ref);
+
+  const onPlacesChanged = () => console.log(this.searchBox.getPlaces());
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyBRor9dsPY8WcfhoMvQM7bHbEXo-NsiUGc">
+    <LoadScript googleMapsApiKey={key} libraries={lib}>
       <GoogleMap
         id="direction-example"
         mapContainerStyle={{
@@ -32,10 +39,10 @@ function Map({ origin, destination }) {
           width: "800px",
         }}
         zoom={10}
-        center={{
-          lat: origin.geometry.location.lat,
-          lng: origin.geometry.location.lng,
-        }}
+        // center={{
+        //   lat: origin.geometry.location.lat,
+        //   lng: origin.geometry.location.lng,
+        // }}
       >
         {destination !== "" && origin !== "" && (
           <DirectionsService
@@ -54,6 +61,27 @@ function Map({ origin, destination }) {
             }}
           />
         )}
+        {/* <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
+          <input
+            type="text"
+            placeholder="Customized your placeholder"
+            style={{
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+              position: "absolute",
+              left: "50%",
+              marginLeft: "-120px",
+            }}
+          />
+        </StandaloneSearchBox> */}
       </GoogleMap>
     </LoadScript>
   );
